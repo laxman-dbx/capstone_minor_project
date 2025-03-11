7611701869a8cd77f67304b1d4b9ba191f61115d
Vethan <21131A05T4@gvpce.ac.in>
2025-03-11 16:56:50 +0530

    minor fixes

--- server/controllers/sharedToMe.js
+++ server/controllers/sharedToMe.js
@@ -10,10 +10,10 @@ exports.sharedToMe = async (req, res) => {
             .select("data")
 
         if (sharedFiles.length === 0) {
-            return res.status(404).json({ message: "No files shared with you." });
+            return res.status(404).json({success : true, message: "No files shared with you." });
         }
 
-        res.status(200).json({ sharedFiles });
+        res.status(200).json({ success : true, sharedFiles });
     } catch (error) {
         res.status(500).json({ error: "An error occurred while fetching shared files." });
     }

303c2815cb883e234008e67ea9bf51550691e6f2
Vethan <21131A05T4@gvpce.ac.in>
2025-03-21 19:20:23 +0530

    files added

---
+++ server/routes/decryption.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const protect = require("../middlewares/authMiddleware");
+const {decryptText} = require("../controllers/decryptText");
+
+
+router.post("/decrypt-text", protect, decryptText);
+
+module.exports = router;
